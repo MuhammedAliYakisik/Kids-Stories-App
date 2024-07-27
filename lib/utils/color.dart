06ad1b6a7fe11d257f82e863e9032018678dc8b4
Muhammed Ali <muhammedali8799@gmail.com>
2024-07-27 16:35:36 +0300

    color page done

---
+++ lib/utils/color.dart
@@ -0,0 +1,13 @@
+import 'dart:ui';
+
+
+//Colors
+const Color blue = Color(0xff195073);
+const Color red = Color(0xffF23207);
+const Color whiteblue = Color(0xff96D2D9);
+const Color orange = Color(0xffF27B35);
+const Color blackred = Color(0xff732509);
+const Color white = Color(0xffF0F0EC);
+const Color grey = Color(0x4A4AECC1);
+
+
