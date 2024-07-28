21b8360b9748a3f747fc0e76af2c36726e26b036
Muhammed Ali <muhammedali8799@gmail.com>
2024-07-28 16:18:09 +0300

    New widgets added to the HomePage

--- lib/ui/views/HomePage.dart
+++ lib/ui/views/HomePage.dart
@@ -27,92 +27,125 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     var oran = MediaQuery.of(context);
     var genislik = oran.size.width;
+    var uzunluk = oran.size.height;
 
     return Scaffold(
-        backgroundColor: whiteblue,
-        appBar: AppBar(
-          title: aramayapiliyormu
-              ? TextField(
-            decoration:
-            InputDecoration(hintText: "Arama için birşey giriniz"),
-            onChanged: (aramasonuc) {
+      backgroundColor: whiteblue,
+      appBar: AppBar(
+        title: aramayapiliyormu
+            ? TextField(
+          decoration: InputDecoration(hintText: "Arama için birşey giriniz"),
+          onChanged: (aramasonuc) {
+            setState(() {
+              aramakelimesi = aramasonuc;
+            });
+            context.read<HomepageCubit>().search(aramasonuc);
+          },
+        )
+            : Text(
+          " ~ Ana Sayfa ~",
+          style: TextStyle(
+              fontFamily: "title",
+              fontWeight: FontWeight.bold,
+              fontSize: genislik / 15),
+        ),
+        centerTitle: true,
+        systemOverlayStyle: SystemUiOverlayStyle.dark,
+        elevation: 0,
+        backgroundColor: Colors.transparent,
+        leading: Icon(
+          Icons.auto_stories_outlined,
+          size: genislik / 15,
+        ),
+        actions: [
+          aramayapiliyormu
+              ? IconButton(
+            icon: Icon(
+              Icons.cancel,
+              color: Colors.black,
+            ),
+            onPressed: () {
               setState(() {
-                aramakelimesi = aramasonuc;
+                aramayapiliyormu = false;
+                aramakelimesi = "";
               });
-              context.read<HomepageCubit>().search(aramasonuc);
+              context.read<HomepageCubit>().fetchshow();
             },
           )
-              : Text(
-            " ~ Ana Sayfa ~",
-            style: TextStyle(
-                fontFamily: "title",
-                fontWeight: FontWeight.bold,
-                fontSize: genislik / 15),
+              : IconButton(
+            icon: Icon(
+              Icons.search,
+              color: Colors.black,
+            ),
+            onPressed: () {
+              setState(() {
+                aramayapiliyormu = true;
+              });
+            },
           ),
-          centerTitle: true,
-          systemOverlayStyle: SystemUiOverlayStyle.dark,
-          elevation: 0,
-          backgroundColor: Colors.transparent,
-          leading: Icon(
-            Icons.auto_stories_outlined,
-            size: genislik / 15,
-          ),
-          actions: [
-            aramayapiliyormu
-                ? IconButton(
-              icon: Icon(
-                Icons.cancel,
-                color: Colors.black,
-              ),
-              onPressed: () {
-                setState(() {
-                  aramayapiliyormu = false;
-                  aramakelimesi = "";
-                });
-                context.read<HomepageCubit>().fetchshow();
-              },
-            )
-                : IconButton(
-              icon: Icon(
-                Icons.search,
-                color: Colors.black,
-              ),
-              onPressed: () {
-                setState(() {
-                  aramayapiliyormu = true;
-                });
-              },
-            )
-          ],
-        ),
-        body: BlocBuilder<HomepageCubit, List<stories>>(
-            builder: (context, storilist) {
-              if (storilist.isNotEmpty) {
-                return ListView.builder(
-                    itemCount: storilist.length,
-                    itemBuilder: (context, indeks) {
-                      var stori = storilist[indeks];
-                      return GestureDetector(
-                        onTap: () {
-                          Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) => DetailPage(stori)));
-                        },
-                        child: ListTile(
-                          title: Text(
-                            "${stori.stori_title}",
-                            style: TextStyle(
-                                fontWeight: FontWeight.bold, color: Colors.black),
+        ],
+      ),
+      body: BlocBuilder<HomepageCubit, List<stories>>(
+        builder: (context, storilist) {
+          if (storilist.isNotEmpty) {
+            return ListView.builder(
+              itemCount: storilist.length,
+              itemBuilder: (context, indeks) {
+                var stori = storilist[indeks];
+                return GestureDetector(
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => DetailPage(stori)),
+                    );
+                  },
+                  child: Padding(
+                    padding: EdgeInsets.symmetric(horizontal: genislik / 16, vertical: 8.0),
+                    child: Card(
+                      elevation: 10,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                      margin: EdgeInsets.all(3),
+                      clipBehavior: Clip.antiAlias,
+                      child: Stack(
+                        children: [
+
+                          SizedBox(height: uzunluk / 3.5,
+                              width: double.infinity,child: Image.asset("assets/image/${stori.stori_image}", fit: BoxFit.cover)),
+                          Positioned(
+                            bottom: 5,
+                            left: 0,
+                            right: 0,
+                            child: Container(
+                              color: Colors.black54,
+                              padding: EdgeInsets.all(8.0),
+                              child: Text(
+                                stori.stori_title,
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: genislik/21,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                                maxLines: 2,
+                                overflow: TextOverflow.ellipsis,
+                              ),
+                            ),
                           ),
-                        ),
-                      );
-                    });
-              } else {
-                return Center(
-                  child: CircularProgressIndicator()
+                        ],
+
+
+                                            ),
+                ),
+                ),
                 );
-              }
-            }));
+              },
+            );
+          } else {
+            return Center(child: CircularProgressIndicator());
+          }
+        },
+      ),
+    );
   }
 }
